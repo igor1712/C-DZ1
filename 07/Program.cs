a692259587cb3693481449eceeee1c0f34b24833
igor <kishm2013@mail.ru>
2022-07-15 18:40:56 +0300

    задача № 15 семинар № 2 с#

---
+++ 07/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day == 6 ^ day == 7)
+{
+    Console.Write("Это выходной день ");
+}
+if(day > 7)
+{
+    Console.Write("В неделе ровно семь дней");
+}
+if(day<6 & day > 0)
+{
+    Console.Write("К сожалению рабочий день");
+}
+if(day == 0)
+{
+    Console.Write("Неделя начинается с первого дня , не с нуля.");
+}
