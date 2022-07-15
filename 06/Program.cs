bcdcfcdfa6e134aa271540c51561dbd0f20efe8a
igor <kishm2013@mail.ru>
2022-07-15 18:39:38 +0300

    задача № 13 семинар № 2 с#

---
+++ 06/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int num ;
+if(a < 99)
+{
+    Console.Write("Это не трехзначное число.");
+}
+if(a > 99 & a < 999 )
+{
+    num = a % 10;
+    Console.WriteLine(num);
+} 
+if(a > 999 & a < 9999)
+{
+    num = a / 10 % 10;
+    Console.WriteLine(num);
+}
+if(a > 9999)
+{
+    num = a / 100 % 10;
+    Console.WriteLine(num);
+}
