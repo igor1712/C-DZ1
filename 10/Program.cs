575875335850b54a0995a6df97e44fd8b01032c3
igor <kishm2013@mail.ru>
2022-07-21 17:46:11 +0300

    Задача № 23 семинар № 3 С#

---
+++ 10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int resault;
+while(num >= count)
+{
+    resault = count * count * count;
+    count++;
+    Console.Write(resault + " ");
+}
