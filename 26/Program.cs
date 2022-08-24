b8b0909649024cecf3c4dc256d8751d560ea185a
igor <kishm2013@mail.ru>
2022-08-24 15:21:45 +0300

    Задача 64 семинар 9

---
+++ 26/Program.cs
@@ -0,0 +1,20 @@
+﻿
+/*Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.*/
+
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Find(int m,int n)
+{
+     if (m>=n)
+     {
+        return m;
+     } 
+   Console.WriteLine(m);
+   return Find(m+1,n);  
+
+   
+}
+Console.WriteLine(Find(m,n));
